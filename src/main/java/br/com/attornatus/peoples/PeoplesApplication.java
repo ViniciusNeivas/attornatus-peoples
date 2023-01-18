d986895e0905f763de268c865d521eeacde4f8f4
ViniciusNeivas <viniciusneivas@outlook.com.br>
2023-01-18 10:02:47 -0300

    Inicialização do projeto com adição das dependências WEB, H2, Devtools, Lombok

---
+++ src/main/java/br/com/attornatus/peoples/PeoplesApplication.java
@@ -0,0 +1,13 @@
+package br.com.attornatus.peoples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeoplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeoplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/attornatus/peoples/PeoplesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.attornatus.peoples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeoplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
