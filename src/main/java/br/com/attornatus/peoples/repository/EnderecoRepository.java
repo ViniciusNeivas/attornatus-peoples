159d711376d7e1b8f2065a7561a2632a96e161a0
ViniciusNeivas <viniciusneivas@outlook.com.br>
2023-01-24 01:51:02 -0300

    Finalização nas funcionalidades básicas e testes, inicio de implemnetação futura de lógica de endereço primário único e ajustes

--- src/main/java/br/com/attornatus/peoples/repository/EnderecoRepository.java
+++ src/main/java/br/com/attornatus/peoples/repository/EnderecoRepository.java
@@ -1,7 +1,6 @@
 package br.com.attornatus.peoples.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import br.com.attornatus.peoples.model.Endereco;
 
 public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
